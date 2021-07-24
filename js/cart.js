7c73e6d64c5968169f1527125af5d93e07b2f714
Alexandre Gicquel <gicquelalexandre94@gmail.com>
2021-07-24 16:46:07 +0200

    systeme de panier refait pour repondre au cahier des charges

--- js/cart.js
+++ js/cart.js
@@ -1,58 +1,64 @@
 let urlApi = 'https://oc-devweb-p5-api.herokuapp.com/api/';
 let urlDummy = 'https://oc-devweb-p5-api.herokuapp.com/api/cameras/5be1ed3f1c9d44000030b061';
 
-let cart = [];
-let sessionKeys = Object.keys(sessionStorage);
+let products = [];
+
 
 
 getCart();
 fillCart();
 
+//Récupère le panier et le stock dans un tableau products
 function getCart () {
-    cart = sessionStorage;
-    console.log(cart);
-    console.log(sessionKeys);
+    products = JSON.parse(sessionStorage.getItem('products'));
+    console.log(products);
 }
 
-function getKey (cartObject) {
-    return '' + cartObject.type + '/' + cartObject.id + '/' + cartObject.option;
-}
-
+// Crée une cart pour chaque element du tableau products
 function fillCart () {
-    let cartObject;
-    for (key of sessionKeys) {
-        cartObject = JSON.parse(sessionStorage.getItem(key));
-        console.log(cartObject);
-        createDivCartObject(cartObject);
+    document.getElementById('total-article').innerHTML = `Nombre total d'article(s) : ${products.length}`;
+    document.getElementById('total-price').innerHTML = `Prix Total : ${calculateTotalPrice()},00 €`;
+    document.getElementById('cart').innerHTML = '';
+    for (product of products) {
+        createDivCartObject(product);
+        //document.getElementById(`delete-${getKey(cartObject)}`).addEventListener('click', deleteFromCart(getKey(cartObject)))
     }
 }
 
-function createDivCartObject (cartObject) {
+//Crée une carte du panier
+function createDivCartObject (product) {
     document.getElementById('cart').innerHTML +=
-    `<div class="card mb-3 bg-light" style="height: 100%; overflow: hidden;">
+    `<div class="card mb-3 bg-light" style="height: 100%; overflow: hidden;" id="${product._id}">
         <div class="row no-gutters">
             <div class="col-4">
-                <img src="${cartObject.imageUrl}" class="card-img rounded-left" alt="...">
+                <img src="${product.imageUrl}" class="card-img rounded-left" alt="...">
             </div>
             <div class="col-8">
                 <div class="card-body">
-                    <h5 class="card-title">${cartObject.name}</h5>
-                    <p class="card-text">${cartObject.price/100},00 €</p>
-                    <span id="quantity-label" class="">
-                        Quantité : 
-                        <input id="quantityInput" type="number" class="form-control col-2" min="1" max="10" value="${cartObject.quantity}">
-                    </span>
-                    <p class="">Total : ${(cartObject.price * cartObject.quantity)/100},00 € </p>
-                    <p class="btn btn-danger text-right">Supprimer</p>
+                    <h5 class="card-title">${product.name}</h5>
+                    <p class="card-text">${product.price/100},00 €</p>
+                    <p class="card-text">Quantité : </p>
+                    <p class="card-text">Total : ,00 € </p>
+                    <p class="btn btn-danger text-right" id="delete-${product._id}">Supprimer</p>
                 </div>
             </div>
         </div>
     </div>`;
+
 }
 
 
 
-function deleteFromCart (cartObject) {
-    sessionStorage.removeItem(getKey(cartObject));
-    Location.reload();
+function deleteFromCart (objectKey) {
+    sessionStorage.removeItem(objectKey);
+    // document.getElementById(objectKey).innerHTML = '';
+    //fillCart();
+}
+
+function calculateTotalPrice () {
+    let totalPrice = 0;
+    for (product of products) {
+        totalPrice += product.price;
+    }
+    return totalPrice/100;
 }
--- js/product.js
+++ js/product.js
@@ -1,18 +1,23 @@
-
-
 let urlDummie = 'https://oc-devweb-p5-api.herokuapp.com/api/cameras/5be1ed3f1c9d44000030b061';
 let urlApi = 'https://oc-devweb-p5-api.herokuapp.com/api/';
+
+//Récupère les données de l'url
 const productType = new URL(location.href).searchParams.get("productType"); 
 const productId = new URL(location.href).searchParams.get("id");
 
+// Récupère les données dans le panier
+// let productsJSON = [];
+
+
+//Crée l'url où faire la requête à l'api
 function getUrl () {
-    // productType = new URL(location.href).searchParams.get("productType");
-    // productId = new URL(location.href).searchParams.get("id");
     return urlApi + productType + '/' + productId;
 }
 
+//Se sert des données de l'url pour afficher un nom d'option personnalisé selon le type de produit
 function displayOptions (product) {
     let optionArray = [];
+    //Determine le type de produit
     if (productType === 'cameras') {
         optionArray = product.lenses;
         document.getElementById('option-label').innerHTML = 'Lentille : ';
@@ -23,22 +28,25 @@ function displayOptions (product) {
         optionArray = product.varnish;
         document.getElementById('option-label').innerHTML = 'Vernis : ';
     }
+    //Remplie le select avec les option propres au produit produit
     let optionSelect = document.getElementById('option-select');
     for (let i = 0 ; i<optionArray.length ; i++){
         optionSelect.innerHTML += `<option value="${i}">${optionArray[i]}</option>`;
     }
 }
 
+//gère l'affichage
 function getById(url) {
+    //récupère les données sur l'api
     fetch(url)
     .then( function (response) {
-        // console.log(response);
         if(response.ok) {
             return response.json();
           } else {
             console.log('Mauvaise réponse du réseau');
           }
     })
+    //Affiche les données
     .then( function (value) {
         // console.log(value);
         document.getElementById('productImg').src = value.imageUrl;
@@ -50,97 +58,27 @@ function getById(url) {
     })
 }
 
-function getKey (cartObject) {
-    return '' + cartObject.type + '/' + cartObject.id + '/' + cartObject.option;
-}
-
-function addToCart (cartObject) {
-    // let sessionStorage = new Storage ();
-    sessionStorage.setItem(getKey(cartObject), JSON.stringify(cartObject));
-    // alert('Le produit a bien été ajouté a votre panier.')
-    // console.log(sessionStorage);
-    
-}
 
 function setupAddToCart (product) {
     document.getElementById('addToCart').addEventListener("click", function () {
-        const option = parseInt(document.getElementById('option-select').value);
-        const quantity = parseInt(document.getElementById('quantityInput').value);
-        let cartObject = new CartObject (productType, product, option, quantity);
-        // console.log(cartObject);
-        checkCart(cartObject);
+
+        productsArr = []
+        let productsJSON = sessionStorage.getItem('products');
+        if (productsJSON === null) {
+            productsArr.push(product);
+            sessionStorage.setItem('products', JSON.stringify(productsArr));
+            console.log(sessionStorage);
+        } else {
+            productsArr = JSON.parse(productsJSON);
+            productsArr.push(product);
+            productsJSON = JSON.stringify(productsArr);
+            sessionStorage.setItem('products', productsJSON);
+            console.log(productsArr);
+        }
+
+        console.log(sessionStorage);
     })
 }
 
 
-function checkCart (cartObject) {
-    let newCartObject = sessionStorage.getItem(getKey(cartObject));
-    // console.log(newCartObject);
-    // console.log(sessionStorage);
-    // console.log(JSON.parse(newCartObject));
-
-    if (newCartObject === null) {
-        addToCart(cartObject);
-    } else {
-        // console.log(JSON.parse(newCartObject));
-        let newCartObjectJS = JSON.parse(newCartObject);
-        newCartObjectJS.quantity += cartObject.quantity;
-        console.log(newCartObjectJS);
-        addToCart(newCartObjectJS);
-
-        
-    }
-
-}
-
-
-
-// console.log(getUrl());
 getById(getUrl());
-
-
-
-
-
-
-
-
-
-
-
-class CartObject {
-    
-
-    constructor (type, element, option, quantity) {
-        this.type = type;
-        this.id = element._id;
-        this.name = element.name;
-        this.descritpion = element.description;
-        this.price = element.price;
-        this.imageUrl = element.imageUrl;
-        this.option = option;
-        this.quantity = quantity;
-    }
-
-    getType() {
-        return this.type;
-    }
-
-    getId() {
-        return this.id;
-    }
-
-    getImgUrl() {
-        return this.imageUrl;
-    }
-
-    getQuantity() {
-        return this.quantity;
-    }
-
-    setQuantity(qt) {
-        this.quantity = qt;
-    }
-
-
-}
